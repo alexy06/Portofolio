068daac403fe929df090f30c07fa41247bf6649b
alexy06 <105698342+alexy06@users.noreply.github.com>
2024-06-08 16:54:06 +0300

    Change 1

---
+++ Portofolio Web Site/PortWebPageScript.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".multiple-text", {
+    strings: ["Software Engineer", "Software Developer", "Ethical Hacker", "Game Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
